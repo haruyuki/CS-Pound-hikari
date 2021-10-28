da0b676b81c0494010f4cccae876c9aac42c6c93
Oliver Lin <jumpy12359@gmail.com>
2021-10-28 20:44:28 +1100

    Add file structure

---
+++ cspound/__main__.py
@@ -0,0 +1,6 @@
+from cspound.core import Bot
+
+bot = Bot()
+
+if __name__ == "__main__":
+    bot.run()
---
+++ cspound/core/bot.py
@@ -0,0 +1,56 @@
+import asyncio
+from pathlib import Path
+import logging
+import os
+import uvloop
+
+from dotenv import load_dotenv
+
+import hikari
+import tanjun
+
+from cspound import GUILD_ID, __version__
+
+from .client import Client
+
+load_dotenv()
+
+logger = logging.getLogger("cspound.main")
+
+
+class Bot(hikari.GatewayBot):
+    def __init__(self) -> None:
+        super().__init__(
+            token=os.environ.get("DISCORD"), logs="INFO", intents=hikari.Intents.ALL
+        )
+
+    def create_client(self) -> None:
+        self.client = Client.from_gateway_bot(self, declare_global_commands=GUILD_ID)
+        self.client.load_modules()
+        self.client.add_prefix("?")
+
+    def run(self: hikari.GatewayBot) -> None:
+        self.create_client()
+
+        self.event_manager.subscribe(hikari.StartingEvent, self.on_starting)
+        self.event_manager.subscribe(hikari.StartedEvent, self.on_started)
+        self.event_manager.subscribe(hikari.StoppingEvent, self.on_stopping)
+
+        super().run()
+
+    async def on_starting(self, event: hikari.StartingEvent) -> None:
+        await self.client.open()
+        logger.info("Bot is starting...")
+
+    async def on_started(self, event: hikari.StartedEvent) -> None:
+        await self.update_presence(
+            activity=hikari.Activity(type=hikari.ActivityType.PLAYING, name="狗狗")
+        )
+        logger.info("Bot is loaded!")
+
+    async def on_stopping(self, event: hikari.StoppingEvent) -> None:
+        await self.client.close()
+        logger.info("Bot has been shut down.")
+
+
+# bot.remove_command("help")
